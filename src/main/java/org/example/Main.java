272d932075cf2a6c87a3a682edbe2c80619a61c1
Unai75 <unai.crupas@educa.jcyl.es>
2026-04-07 12:46:02 +0200

    Añado comentarios

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ import java.sql.*;
 public class Main {
 
     public static void main(String[] args) {
+        //Creamos la sentencia sql donde mostramos los datos del empledo y el id y nombre del departamento con el id 10
         String sql = "SELECT EMPLEADO.ID AS empleado_id,EMPLEADO.NOMBRE as empleado_nombre, EMPLEADO.SALARIO AS salario, EMPLEADO.DEPARTAMENTO_ID AS idDepartamento, DEPARTAMENTO.NOMBRE as departamento_nombre " +
                 "FROM EMPLEADO JOIN DEPARTAMENTO ON EMPLEADO.DEPARTAMENTO_ID=DEPARTAMENTO.ID " +
                 "WHERE DEPARTAMENTO.ID=10";
@@ -14,11 +15,12 @@ public class Main {
                 DBConfig.getUrl(),
                 DBConfig.getUser(),
                 DBConfig.getPassword());
+             //Ponemos aqui los stament y resultset en el try with resources para que se cierren automaticamente
              Statement stmt = conn.createStatement();
              ResultSet resultSet = stmt.executeQuery(sql)) {
             System.out.println("Conectado con suceso!");
 
-
+//Ahora en el resultSet lo ponemos en un bucle para que nos muestre toda la información de todos los empleados
             while (resultSet.next()) {
                 int id = resultSet.getInt("empleado_id");
 
